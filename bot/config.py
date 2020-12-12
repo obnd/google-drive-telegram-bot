58122f349ed0f266c2113d9a56c82651a26b533f
obnd <75868716+obnd@users.noreply.github.com>
2020-12-12 08:59:18 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 class config:
-    BOT_TOKEN = "1474570742:AAHdfNxFncqsYvsqPdazDW10uJ8AKMBgrqQ"
-    APP_ID = "2820796"
-    API_HASH = "4469a1e47933b8cff4d738bfbdf33043"
+    BOT_TOKEN = "1468328385:AAGELtGyY6iBE8viQvzaxeVxQmne9iGMJww"
+    APP_ID = "2121142"
+    API_HASH = "73aea46b39f24bd55f967990926c32e9"
     DATABASE_URL = "postgres://bmzynszpeidykl:138f8b2bce96e1384cd2e3bc7c3e50c1dae067204232b017ff010deae8ca1b77@ec2-18-206-103-49.compute-1.amazonaws.com:5432/d8ejimlmcr5kbd"
     SUDO_USERS = "1019132655 1360404905" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/dongoc"
